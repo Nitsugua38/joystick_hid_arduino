525b307d6d15dc3f58233e9b6714f28150e84e2c
Nitsugua38 <58813858+Nitsugua38@users.noreply.github.com>
2024-03-16 16:42:24 +0000

    Update screen_joystick.ino

--- screen_joystick.ino
+++ screen_joystick.ino
@@ -1,29 +1,15 @@
-#include <LiquidCrystal.h>
-
-const int rs=2, en=3, d4=4, d5=5, d6=6, d7=7;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
-
-
 const int pinX = A0;
 const int pinY = A1;
 const int pinBouton = 9;
 
 
 
-void setup() {
-  lcd.clear();
-  lcd.begin(16, 2);
-  lcd.print("hello, world !");
-  
+void setup() {  
   pinMode(pinBouton , INPUT_PULLUP);
   Serial.begin(9600);
 }
 
 void loop() {
-  lcd.display();
-
-
   int X = 0;
   int Y = 0;
   bool boutonValeur = false;
